984ed187200425b554ef720bc0d3773a1d09d741
Mohammed Bouayach <mrifi703@gmail.com>
2026-02-08 23:52:35 +0100

    Remove next-pwa conflict

--- next.config.js
+++ next.config.js
@@ -1,19 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
   images: {
-    domains: ['d1.islamhouse.com', 'via.placeholder.com'],
+    domains: [
+      'd1.islamhouse.com',
+      'via.placeholder.com',
+    ],
   },
+
   async headers() {
     return [
       {
         source: '/sw.js',
         headers: [
-          { key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' },
-          { key: 'Service-Worker-Allowed', value: '/' },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          },
         ],
       },
     ];
+  },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
   },
 };
 
